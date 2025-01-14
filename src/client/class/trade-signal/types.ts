87a7024a2d26a898bcc74b9580ee4082939e19a8
Manny <eqo5037@gmail.com>
2025-01-14 07:54:29 +0000

    add workflow actions

--- src/client/class/trade-signal/types.ts
+++ src/client/class/trade-signal/types.ts
@@ -1,4 +1,4 @@
-import { TradeSignal } from "@tradingbot/lib";
+import { TradeSignal } from "@duabalabs/lib-tradingbot";
 import Parse from "parse";
 
 export type ITradeSignal = Parse.Object & TradeSignal;
