93011ff9703954e252e12a157679e07218ff9e61
Manny <eqo5037@gmail.com>
2025-01-17 00:06:48 +0000

    updae

--- src/client/class/index.ts
+++ src/client/class/index.ts
@@ -1,5 +1,4 @@
 import { SubClass } from "../initialize";
-import { TradeSignal, TRADESIGNAL_CLASSNAME } from "./trade-signal";
 import { User, USER_CLASSNAME } from "./user";
 
 const CustomSubClasses: SubClass[] = [
@@ -7,13 +6,8 @@ const CustomSubClasses: SubClass[] = [
     className: USER_CLASSNAME,
     class: User as any,
   },
-  {
-    className: TRADESIGNAL_CLASSNAME,
-    class: TradeSignal as any,
-  },
 ];
 export const SubClasses: SubClass[] = [...CustomSubClasses];
 
 export * from "./chat";
-export * from "./trade-signal";
 export * from "./user";
--- src/client/class/trade-signal/types.ts
+++
@@ -1,4 +0,0 @@
-import { TradeSignal } from "@duabalabs/lib-tradingbot";
-import Parse from "../../../parse.js";
-
-export type ITradeSignal = Parse.Object & TradeSignal;
